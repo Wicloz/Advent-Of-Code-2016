a84d5583bf126d0b1a0786ea561053c9478f0a56
Wilco de Boer <wfh@wilcodeboer.me>
2016-12-02 12:12:09 +0100

    Day 2, part 2

--- day 2/main.py
+++ day 2/main.py
@@ -1,16 +1,28 @@
-lr = 1
-ud = 1
+lr = 0
+ud = 2
+
+keypad = [
+    ['x', 'x', '1', 'x', 'x'],
+    ['x', '2', '3', '4', 'x'],
+    ['5', '6', '7', '8', '9'],
+    ['x', 'A', 'B', 'C', 'x'],
+    ['x', 'x', 'D', 'x', 'x'],
+]
 
 input = open('input.txt', 'r')
 
 for move in input:
     for step in move:
-        if step is 'D' and ud < 2:
-            ud += 1
+        if step is 'D' and ud < 4:
+            if keypad[ud+1][lr] is not 'x':
+                ud += 1
         if step is 'U' and ud > 0:
-            ud -= 1
-        if step is 'R' and lr < 2:
-            lr += 1
+            if keypad[ud-1][lr] is not 'x':
+                ud -= 1
+        if step is 'R' and lr < 4:
+            if keypad[ud][lr+1] is not 'x':
+                lr += 1
         if step is 'L' and lr > 0:
-            lr -= 1
-    print(ud * 3 + lr + 1)
+            if keypad[ud][lr-1] is not 'x':
+                lr -= 1
+    print(keypad[ud][lr])
