2aa3c009409a2cc6d733c1b1064bee31fea38dcd
Wilco de Boer <wfh@wilcodeboer.me>
2016-12-03 12:04:12 +0100

    Day 3, part 2

--- day 3/main.py
+++ day 3/main.py
@@ -1,17 +1,27 @@
 count = 0
 input = open('input.txt', 'r')
 
+sets = [[], [], []]
+
 for line in input:
     sides = line[:-1].split()
-    possible = True
-    if int(sides[0]) >= int(sides[1]) + int(sides[2]):
-        possible = False
-    if int(sides[1]) >= int(sides[0]) + int(sides[2]):
-        possible = False
-    if int(sides[2]) >= int(sides[0]) + int(sides[1]):
-        possible = False
-    if possible:
-        count += 1
+    sets[0] += [int(sides[0])]
+    sets[1] += [int(sides[1])]
+    sets[2] += [int(sides[2])]
+
+    if len(sets[0]) == 3:
+        for i in range(0, 3):
+            possible = True
+            if sets[i][0] >= sets[i][1] + sets[i][2]:
+                possible = False
+            if sets[i][1] >= sets[i][0] + sets[i][2]:
+                possible = False
+            if sets[i][2] >= sets[i][0] + sets[i][1]:
+                possible = False
+            if possible:
+                count += 1
+
+        sets = [[], [], []]
 
 print(count)
 
