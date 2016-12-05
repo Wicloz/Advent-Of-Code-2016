226c82d54cbd5c8ce0b3197515e7b23daf8f7b89
Wilco de Boer <wfh@wilcodeboer.me>
2016-12-05 18:15:53 +0100

    Less is more

--- day 5/main.py
+++ day 5/main.py
@@ -1,5 +1,4 @@
 import hashlib
-import random
 
 input = 'wtnhxymk'
 password = '________'
@@ -7,12 +6,7 @@ password = '________'
 index = 0
 while '_' in password:
     hash = hashlib.md5((input + str(index)).encode('utf-8')).hexdigest()
-    good_hash = True
-    for i in range(0, 5):
-        if hash[i] is not '0':
-            good_hash = False
-            break
-    if good_hash and hash[5].isdigit() and int(hash[5]) in range(0, 8) and password[int(hash[5])] is '_':
+    if hash.startswith('00000') and hash[5].isdigit() and int(hash[5]) in range(0, 8) and password[int(hash[5])] is '_':
         password = password[:int(hash[5])] + hash[6] + password[int(hash[5])+1:]
         print(password)
     index += 1
