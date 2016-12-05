e10ce6b72257434df886a14a5a7986ccca2e17b6
Wilco de Boer <wfh@wilcodeboer.me>
2016-12-05 17:19:26 +0100

    Day 5, part 2

--- day 5/main.py
+++ day 5/main.py
@@ -1,19 +1,20 @@
 import hashlib
+import random
 
 input = 'wtnhxymk'
-password = ''
+password = '________'
 
 index = 0
-while len(password) < 8:
-    print(index)
+while '_' in password:
     hash = hashlib.md5((input + str(index)).encode('utf-8')).hexdigest()
     good_hash = True
     for i in range(0, 5):
         if hash[i] is not '0':
             good_hash = False
             break
-    if good_hash:
-        password += hash[5]
+    if good_hash and hash[5].isdigit() and int(hash[5]) in range(0, 8) and password[int(hash[5])] is '_':
+        password = password[:int(hash[5])] + hash[6] + password[int(hash[5])+1:]
+        print(password)
     index += 1
 
-print(password)
+print(index)
