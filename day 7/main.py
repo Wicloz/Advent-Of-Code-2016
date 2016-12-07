c73157ffc89bcd53f667ac2120658ab747a761d2
Wilco de Boer <wfh@wilcodeboer.me>
2016-12-07 13:19:56 +0100

    Begin part 2

--- day 7/main.py
+++ day 7/main.py
@@ -1,19 +1,12 @@
-import re
-
-def has_abba(word):
-    for i in range(0, len(word) - 3):
-        if word[i] is word[i+3] and word[i+1] is word[i+2] and word[i] is not word[i+1]:
-            return True
-    return False
-
 input = open('input.txt', 'r')
 count = 0
 
 for line in input:
-    outside = list(filter(None, re.findall(r"(.*?)(?:\[.*?\]|$)", line)))
-    inside = re.findall(r"\[(.*?)\]", line)
-    if sum([has_abba(w) for w in outside]) > 0 and not sum([has_abba(w) for w in inside]) > 0:
-        count += 1
+    abas = []
+    in_brackets = False
+    for i in range(0, len(line) - 2):
+        if (line[i] is '[' and not in_brackets) or (line[i] is ']' and in_brackets):
+            in_brackets = not in_brackets
 
 print(count)
 input.close()
