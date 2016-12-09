9552af7cfdf09b3b074f573938d3bf4f3a4b3bbb
Wilco de Boer <wfh@wilcodeboer.me>
2016-12-09 13:46:10 +0100

    Improve part 1

--- day 9/main.py
+++ day 9/main.py
@@ -16,18 +16,20 @@ for i in range(len(compressed)):
     if amount > 0:
         rep_string += compressed[i]
         amount -= 1
-    elif repeats > 0:
-        while repeats > 1:
+    if amount == 0 and repeats > 0:
+        while repeats > 0:
             decompressed += rep_string
             repeats -= 1
+        continue
 
-    if amount == 0 and compressed[i] is '(':
-        amount, repeats = [int(x) for x in compressed[i+1:compressed.find(')', i)].split('x')]
-        skip = compressed.find(')', i) + 1
-        rep_string = ''
+    if amount == 0:
+        if compressed[i] is '(':
+            amount, repeats = [int(x) for x in compressed[i+1:compressed.find(')', i)].split('x')]
+            skip = compressed.find(')', i) + 1
+            rep_string = ''
 
-    elif compressed[i] is not '\n':
-        decompressed += compressed[i]
+        elif compressed[i] is not '\n':
+            decompressed += compressed[i]
 
 print(decompressed)
 print(len(decompressed))
