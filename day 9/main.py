dadd68dfce7cb4d4c2b311778b02d4c85f5d607e
Wilco de Boer <wfh@wilcodeboer.me>
2016-12-09 12:49:36 +0100

    Day 9, part 1

---
+++ day 9/main.py
@@ -0,0 +1,33 @@
+input = open('input.txt', 'r')
+
+compressed = input.read()
+decompressed = ''
+input.close()
+
+amount = 0
+repeats = 0
+rep_string = ''
+
+skip = 0
+for i in range(len(compressed)):
+    if i < skip:
+        continue
+
+    if amount > 0:
+        rep_string += compressed[i]
+        amount -= 1
+    elif repeats > 0:
+        while repeats > 1:
+            decompressed += rep_string
+            repeats -= 1
+
+    if amount == 0 and compressed[i] is '(':
+        amount, repeats = [int(x) for x in compressed[i+1:compressed.find(')', i)].split('x')]
+        skip = compressed.find(')', i) + 1
+        rep_string = ''
+
+    elif compressed[i] is not '\n':
+        decompressed += compressed[i]
+
+print(decompressed)
+print(len(decompressed))
