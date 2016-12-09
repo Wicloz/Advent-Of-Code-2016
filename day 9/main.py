b31391894a5d0e71f95269db11ae7288a71dbc94
Wilco de Boer <wfh@wilcodeboer.me>
2016-12-09 16:20:33 +0100

    Day 9, part 2

--- day 9/main.py
+++ day 9/main.py
@@ -1,35 +1,22 @@
-input = open('input.txt', 'r')
+def count_decompressed(compressed):
+    length = 0
+    skip = 0
+    for i in range(len(compressed)):
+        if i < skip:
+            continue
+        if compressed[i] is '(':
+            close = compressed.find(')', i)
+            chars, repeats = [int(x) for x in compressed[i+1:close].split('x')]
+            skip = close + chars + 1
+            block = compressed[close+1:skip]
+            length += repeats*count_decompressed(block)
+        elif compressed[i] is not '\n':
+            length += 1
+    return length
 
+input = open('input.txt', 'r')
 compressed = input.read()
-decompressed = ''
 input.close()
 
-amount = 0
-repeats = 0
-rep_string = ''
-
-skip = 0
-for i in range(len(compressed)):
-    if i < skip:
-        continue
-
-    if amount > 0:
-        rep_string += compressed[i]
-        amount -= 1
-    if amount == 0 and repeats > 0:
-        while repeats > 0:
-            decompressed += rep_string
-            repeats -= 1
-        continue
-
-    if amount == 0:
-        if compressed[i] is '(':
-            amount, repeats = [int(x) for x in compressed[i+1:compressed.find(')', i)].split('x')]
-            skip = compressed.find(')', i) + 1
-            rep_string = ''
-
-        elif compressed[i] is not '\n':
-            decompressed += compressed[i]
-
-print(decompressed)
-print(len(decompressed))
+length = count_decompressed(compressed)
+print(length)
